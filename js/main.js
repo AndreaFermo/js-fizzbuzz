9fdfa10bb1e9e00c58d09b1450dec964fd798f66
Andrea Fermo <andreafermo.sd@gmail.com>
2023-02-24 14:51:34 +0100

    esercizio concluso

--- js/main.js
+++ js/main.js
@@ -1,15 +1,30 @@
+const containerDom = document.querySelector('.container');
 
 for (let i = 1; i <= 100; i++) {
+
+    const quadratino = document.createElement('div');
+    quadratino.classList.add('square');
     
-if (i % 5 == 0 && i % 3 == 0 ) {
-    console.log("fizzbuzz");
-} else if (i % 5 ==0) {
-    console.log("buzz");
-} else if (i % 3 ==0) {
-    console.log("fizz")
-} else {
-    console.log(i);
-}
+    containerDom.append(quadratino);
+        
+    if (i % 5 == 0 && i % 3 == 0 ) {
+        console.log("fizzbuzz");
+        quadratino.append("fizzbuzz");
+        quadratino.classList.add('bgp');
+    } else if (i % 5 ==0) {
+        quadratino.append("buzz");
+        quadratino.classList.add('bgy');
+        console.log("buzz");
+    } else if (i % 3 ==0) {
+        quadratino.append("fizz");
+        quadratino.classList.add('bgg');
+        console.log("fizz")
+    } else {
+        quadratino.append(i);
+        console.log(i);
+    }
 
 }
+
+
 
